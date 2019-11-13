fa3a6c04e3a403217bd8cf0e2a268882fdbbe8ec
gattidanielmarco <gatti.daniel.marco@iisgalvanimi.edu.it>
2019-11-13 10:45:33 +0000

    Lego

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -10,6 +10,13 @@ app.get('/', function (req, res) {
    people: people.profiles //Passa il vettore profiles alla pagina index.pug
  });
 });
+app.get('/profile', (req, res) => {
+  const person = people.profiles.find((p) => p.id === req.query.id);
+  res.render('profile', {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
